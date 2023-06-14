d7073a108dd72908ed3c5bef2bd95f2c35f86d1c
Esteban93040 <espinosa.esteban@correounivalle.edu.co>
2023-06-13 20:26:14 -0500

    Initial commit

---
+++ src/resources/filesManager.java
@@ -0,0 +1,10 @@
+package resources;
+
+import java.io.*;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class filesManager {
+
+}
---
+++ src/resources/mainMenu.java
@@ -0,0 +1,22 @@
+package resources;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This class is used for ...
+ * @autor Esteban Andres Espinosa Aragon (espinosa.esteban@correounivalle.edu.co
+ *        Valentina Sanchez Rosero (valentina.sanchez.rosero@correounivalle.edu.co
+ * @version v.1.0.0 date:13/0/2023
+ */
+public class mainMenu extends JFrame {
+
+    
+}
