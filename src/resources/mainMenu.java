52e5bbb1f183f5814cc6886ba09716ce73bc6772
Esteban93040 <espinosa.esteban@correounivalle.edu.co>
2023-06-13 20:30:32 -0500

    Inicializacion de la clase Main

--- src/resources/mainMenu.java
+++ src/resources/mainMenu.java
@@ -17,6 +17,24 @@ import java.util.List;
  * @version v.1.0.0 date:13/0/2023
  */
 public class mainMenu extends JFrame {
+    public mainMenu(){
+        initGUI();
+        setIconImage(new ImageIcon(getClass().getResource("/resources/Imagen1.jpg")).getImage());
+        //Default JFrame configuration
+        this.setTitle("I Know That Word");
+        this.setSize(1020,720);
+        this.getContentPane();
+        this.setResizable(true);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    private void initGUI(){
 
-    
+    }
+    public static void main(String[] args){
+        EventQueue.invokeLater(() -> {
+            mainMenu miProjectGUI = new mainMenu();
+        });
+    }
 }
