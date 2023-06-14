d9de19cae04064cd1bfc2d0af92591f469e5cde2
Esteban93040 <espinosa.esteban@correounivalle.edu.co>
2023-06-13 20:32:35 -0500

    Creacion de la imagen de fondo para el menu principal

--- src/resources/mainMenu.java
+++ src/resources/mainMenu.java
@@ -17,6 +17,8 @@ import java.util.List;
  * @version v.1.0.0 date:13/0/2023
  */
 public class mainMenu extends JFrame {
+    private JPanel principalPanel,buttonPanel,counterPanel,backgroundPanel,inGameBackgroundPanel;
+    private Image backgroundTest,backgroundTest2;
     public mainMenu(){
         initGUI();
         setIconImage(new ImageIcon(getClass().getResource("/resources/Imagen1.jpg")).getImage());
@@ -30,6 +32,33 @@ public class mainMenu extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     private void initGUI(){
+        backgroundPanel = new JPanel(){
+            protected void paintComponent(Graphics g){
+                super.paintComponent(g);
+                if(backgroundTest!=null){
+                    //int x = (getWidth()-backgroundTest.getWidth(null))/2;
+                    //int y = (getHeight()-backgroundTest.getHeight(null))/2;
+                    g.drawImage(backgroundTest,0,0,getWidth(),getHeight(),null);
+                }
+
+            }
+        };
+        backgroundPanel.setPreferredSize(new Dimension(1400,1080));
+
+        //Creacion de hilo
+
+        Thread hilo= new Thread(){
+            @Override
+            public void run(){
+                try {
+                    backgroundTest= ImageIO.read(getClass().getResource("/resources/12.jpg"));
+                    backgroundPanel.repaint();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        hilo.start();
 
     }
     public static void main(String[] args){
